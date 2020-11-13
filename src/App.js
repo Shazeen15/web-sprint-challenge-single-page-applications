bcb1dc957c7556060f0c7651865cda68d077cf2b
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2020-11-13 09:17:38 -0800

    adding home and pizza components and set up lay out of page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import {Link, Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Pizza from './components/Pizza';
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/pizza'>Pizza</Link>
+      </nav>
+      <Switch>
+        <Route path='/pizza'>
+          <Pizza/>
+        </Route>
+        <Route path='/'>
+          <Home/>
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
---
+++ src/components/Pizza.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Pizza(){
+    return(
+        <div>
+            <h1>Pizza Page</h1>
+        </div>
+    )
+}
+
+export default Pizza;
